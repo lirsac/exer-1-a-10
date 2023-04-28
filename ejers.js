154ad6c5eea0dabd54bab63dc406082230a6d144
lirsac <lukitasromo@gmail.com>
2023-04-28 04:31:45 -0300

    se agrega proyecto

---
+++ ejers.js
@@ -0,0 +1,25 @@
+let miNombre = "Lucas"
+
+let miApellido = "Romo"
+
+let miEdad = 19
+
+let miMascota = "Pompona"
+
+let edadMascota = 3
+
+console.log(miNombre, miApellido, miEdad, miMascota, edadMascota)
+
+let nombreCompleto = miNombre + miApellido
+
+
+let textoPresentacion = `${miNombre} ${miApellido} ${miEdad} ${miMascota} ${edadMascota}`
+
+let sumaEdades = miEdad + edadMascota
+
+let restaEdades = miEdad - edadMascota
+
+let productoEdades = miEdad * edadMascota
+
+let divisionEdades = miEdad / edadMascota
+
